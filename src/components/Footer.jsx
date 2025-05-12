58be986f3873b38620c39792b1829ce3b6269abd
Lucas Pujol <lucas.pujol@epitech.eu>
2025-05-12 12:00:10 +0200

    [UPDATE] Fix LinkedIn URL in Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <p>&copy; {new Date().getFullYear()} Lucas Pujol. All rights reserved.</p>
             <div>
                 <a href="https://github.com/https://github.com/lucaspujol" target="_blank" rel="noopener noreferrer">GitHub</a>
-                <a href="https://https://www.linkedin.com/in/lucas-pujol-epitech/.com/in/yourprofile" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                <a href="https://www.linkedin.com/in/lucas-pujol-epitech/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
                 <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer">Twitter</a>
             </div>
         </footer>
